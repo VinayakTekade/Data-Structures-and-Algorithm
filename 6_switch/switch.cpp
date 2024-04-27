d32c226dd32fd19562f8976d5fd3046e93215068
Vinayak Tekade <39454332+VinayakTekade@users.noreply.github.com>
2024-04-27 18:41:02 +0530

    revise switch

--- 6_switch/switch.cpp
+++ 6_switch/switch.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../compareFiles.h"
 using namespace std;
+
+void generateResult()
+{
+    bool result = compareFiles("../expected_output.txt", "../output.txt");
+    result ? cerr << "Test Case Passed!" << endl : cerr << "Test Case Failed!" << endl;
+}
 
 int main()
 {
@@ -8,6 +17,7 @@ int main()
     freopen("../output.txt", "w", stdout);
 #endif
 
+    // ============ Your Code goes here ==================
     int n1, n2;
     cin >> n1 >> n2;
 
@@ -26,6 +36,7 @@ int main()
 
     case '*':
         cout << n1 * n2;
+        break;
 
     case '/':
         cout << n1 / n2;
@@ -40,5 +51,58 @@ int main()
         break;
     }
 
+    // ============ Your Code ends here ==================
+#ifndef ONLINE_JUDGE
+    fclose(stdout);
+#endif
+    generateResult();
     return 0;
 }
+
+// ==== TEST CASE 1 =====
+// ---- INPUT ----
+// 20
+// 30
+// +
+// ---- OUTPUT ----
+// 50
+
+// ==== TEST CASE 2 =====
+// ---- INPUT ----
+// 30
+// 20
+// -
+// ---- OUTPUT ----
+// 10
+
+// ==== TEST CASE 3 =====
+// ---- INPUT ----
+// 100
+// 223
+// *
+// ---- OUTPUT ----
+// 22300
+
+// ==== TEST CASE 4 =====
+// ---- INPUT ----
+// 100
+// 5
+// /
+// ---- OUTPUT ----
+// 20
+
+// ==== TEST CASE 5 =====
+// ---- INPUT ----
+// 100
+// 3
+// %
+// ---- OUTPUT ----
+// 1
+
+// ==== TEST CASE 6 =====
+// ---- INPUT ----
+// 100
+// 3
+// ^
+// ---- OUTPUT ----
+// Invalid Operator
--- 6_switch/vowelConsonant.cpp
+++ 6_switch/vowelConsonant.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "../compareFiles.h"
 using namespace std;
+
+void generateResult()
+{
+    bool result = compareFiles("../expected_output.txt", "../output.txt");
+    result ? cerr << "Test Case Passed!" << endl : cerr << "Test Case Failed!" << endl;
+}
 
 int main()
 {
@@ -8,6 +17,7 @@ int main()
     freopen("../output.txt", "w", stdout);
 #endif
 
+    // ============ Your Code goes here ==================
     char op;
     cin >> op;
 
@@ -40,5 +50,34 @@ int main()
         break;
     }
 
+    // ============ Your Code ends here ==================
+#ifndef ONLINE_JUDGE
+    fclose(stdout);
+#endif
+    generateResult();
     return 0;
 }
+
+// ==== TEST CASE 1 =====
+// ---- INPUT ----
+// a
+// ---- OUTPUT ----
+// a is a vowel
+
+// ==== TEST CASE 2 =====
+// ---- INPUT ----
+// b
+// ---- OUTPUT ----
+// b is a consonant
+
+// ==== TEST CASE 3 =====
+// ---- INPUT ----
+// e
+// ---- OUTPUT ----
+// e is a vowel
+
+// ==== TEST CASE 4 =====
+// ---- INPUT ----
+// f
+// ---- OUTPUT ----
+// f is a consonant
